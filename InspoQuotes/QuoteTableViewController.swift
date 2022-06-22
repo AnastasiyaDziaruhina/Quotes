43daf9b96410c50c4de3ff44f8c389b0088170a4
stacysfish <st.vitalievna@yandex.ru>
2022-06-22 15:23:37 +0200

    Give access to purchased items

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -9,58 +9,63 @@ import UIKit
 import StoreKit
 
 class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
-   
+    
     
     let productID = ""
     
     var quotesToShow = [
-            "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
-            "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
-            "It does not matter how slowly you go as long as you do not stop. – Confucius",
-            "Everything you’ve ever wanted is on the other side of fear. — George Addair",
-            "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
-            "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
-        ]
-        
-        let premiumQuotes = [
-            "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
-            "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
-            "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
-            "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
-            "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
-            "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
-        ]
-
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         SKPaymentQueue.default().add(self)
-
+        
+        if isPurchased() {
+            showPremiumQuotes()
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         
         guard let navBar = navigationController?.navigationBar else {
-        fatalError("Navigation controller does not exist")
+            fatalError("Navigation controller does not exist")
         }
-         
+        
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = #colorLiteral(red: 0.8416352272, green: 0.6825858951, blue: 1, alpha: 1)
         appearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         navBar.standardAppearance = appearance;
         navBar.scrollEdgeAppearance = navBar.standardAppearance
-     
-        }
-
-    // MARK: - Table view data source
-
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-        // #warning Incomplete implementation, return the number of rows
         
-        return quotesToShow.count + 1
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isPurchased() {
+            return quotesToShow.count
+        } else{
+            return quotesToShow.count + 1
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -70,6 +75,8 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
         if indexPath.row < quotesToShow.count {
             cell.textLabel?.text = quotesToShow[indexPath.row]
             cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            cell.accessoryType = .none
         } else {
             cell.textLabel?.text = "Get more quotes"
             cell.textLabel?.textColor = #colorLiteral(red: 0.5810584426, green: 0.1285524964, blue: 0.5745313764, alpha: 1)
@@ -78,7 +85,7 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
         
         return cell
     }
-
+    
     //MARK: - Table view delegate methdos
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -100,11 +107,9 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
             
         } else {
             //can't make payments
-            
         }
-        
     }
-
+    
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         for transaction in transactions {
             if transaction.transactionState == .purchased {
@@ -117,14 +122,32 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
                 //payment failed
                 print("print not ok")
                 
+                showPremiumQuotes()
+                UserDefaults.standard.set(true, forKey: productID) // isPurchased or not?
+                
                 if let error = transaction.error {
                     let errorDescriptons = error.localizedDescription
                     print("failed due to error \(errorDescriptons)")
                 }
                 
                 SKPaymentQueue.default().finishTransaction(transaction)
-
             }
+        }
+    }
+    
+    func showPremiumQuotes() {
+        quotesToShow.append(contentsOf: premiumQuotes)
+        tableView.reloadData()
+    }
+    
+    func isPurchased() -> Bool {
+        let purchaseStatus = UserDefaults.standard.bool(forKey: productID)
+        if purchaseStatus {
+            print("Previously purchased")
+            return true
+        }else {
+            print("Never purchased")
+            return false
         }
     }
     
@@ -132,5 +155,5 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
     }
     
-
+    
 }
